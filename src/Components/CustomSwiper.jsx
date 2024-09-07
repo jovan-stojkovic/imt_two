3df787f9059a7ccff346bcae6bf1aebbbf7201fb
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-09-07 15:29:14 +0300

    extra slider

--- src/Components/CustomSwiper.jsx
+++ src/Components/CustomSwiper.jsx
@@ -6,21 +6,28 @@ import "swiper/scss/navigation";
 import "swiper/scss/autoplay";
 import "../Styles/CustomSwiper.scss";
 
+const headline = "IMT TRAKTORI";
+
 const slides = [
+  { image: "/backgrounds/Home.png" },
   {
     image: "/swiper/jm00.jpg",
+    headline: headline,
     text: "INDUSTRIJA MEHANIZACIJE I TRAKTORA je ekskluzivni distributer kompanije Tractors and Farm Equipment Limited - TAFE za brend traktora IMT i TAFE za tržište Srbije.",
   },
   {
     image: "/swiper/jm01.jpg",
+    headline: headline,
     text: "Sklapamo traktore u pogonima IMT u Jarkovcu, opština Sečanj. Traktori se dopremaju u nesastavljenom stanju i namenjeni su za industrijsku montažu.",
   },
   {
     image: "/swiper/jm02.jpg",
+    headline: headline,
     text: "Najbolji izbor za sve Vaše potrebe! Traktori od 26KS - 85KS za korišćenje u ratarstvu, vinogradarstvu kao i komunalnoj delatnosti.",
   },
   {
     image: "/swiper/jm03.jpg",
+    headline: headline,
     text: "Najbolji izbor za sve Vaše potrebe! Garantni period od dve godine ili 2000h.",
   },
 ];
@@ -42,10 +49,12 @@ const CustomSwiper = () => {
         <SwiperSlide key={index}>
           <div className="slide-content">
             <img src={slide.image} alt="" />
-            <div className="slide-text">
-              <h2>IMT TRAKTORI</h2>
-              {slide.text}
-            </div>
+            {slide.headline && (
+              <div className="slide-text">
+                <h2>{slide.headline}</h2>
+                {slide.text}
+              </div>
+            )}
           </div>
         </SwiperSlide>
       ))}
