f053368e97fd340bdd52d84cf138a3ea7843d7f1
jovan-stojkovic <code.jovanstojkovic@gmail.com>
2024-09-11 12:47:49 +0200

    made nav

--- src/Pages/Download.jsx
+++ src/Pages/Download.jsx
@@ -26,7 +26,33 @@ const Download = () => {
         <h1>Preuzimanja</h1>
       </motion.div>
 
-      <div className="page-cont"></div>
+      <div className="page-cont">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem vero
+          voluptates dolorum, error fuga nulla? Aperiam, rem omnis, provident
+          mollitia suscipit reiciendis tenetur magnam enim, sequi reprehenderit
+          magni? Enim unde, officiis doloremque expedita dicta cupiditate. Ipsum
+          quibusdam vero, est reiciendis voluptas voluptatibus, beatae expedita
+          delectus placeat commodi blanditiis reprehenderit corrupti iste sit,
+          repellat quisquam dolore quos id tenetur dolores assumenda praesentium
+          enim nam quis. Temporibus error dolorum pariatur voluptatibus hic
+          perspiciatis sed non excepturi inventore ipsam voluptas minus
+          cupiditate libero ducimus quaerat maxime vero quas illum suscipit
+          corrupti id labore, fugiat dignissimos nam. Magnam unde veritatis non
+          impedit quidem dolores!
+        </p>
+
+        <div className="bar">
+          <div className="red-patch"></div>
+          <div className="red-part"></div>
+          <div className="gray-part">
+            <div className="gray-triangle"></div>
+          </div>
+          <div className="transparent-part">
+            <div className="logo-cont"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
